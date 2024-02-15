4347a56bea4a7b988916e9f015e84eda61471327
hnamzian <namazian87@gmail.com>
2024-02-15 15:21:17 -0500

    Created PedersenKey and KeyManger interfaces

---
+++ pkg/common/cryptosuite/pedersen/pedersen.go
@@ -0,0 +1,34 @@
+package pedersen
+
+import "github.com/cronokirby/saferith"
+
+type PedersenKey interface {
+	// Bytes returns the byte representation of the key.
+	Bytes() ([]byte, error)
+
+	// SKI returns the serialized key identifier.
+	SKI() []byte
+
+	// Private returns true if the key is private.
+	Private() bool
+
+	// PublicKey returns the corresponding public key part of Pedersen Key.
+	PublicKey() PedersenKey
+}
+
+type PedersenKeyManger interface {
+	// GenerateKey generates a new Pedersen key pair.
+	GenerateKey() (PedersenKey, error)
+
+	// ImportKey imports a Pedersen key.
+	ImportKey(key PedersenKey) error
+
+	// GetKey returns a Pedersen key by its SKI.
+	GetKey(ski []byte) (PedersenKey, error)
+
+	// Commit returns the commitment of the given value.
+	Commit(ski []byte, x, y *saferith.Int) *saferith.Nat
+
+	// Verify returns true if the given commitment is valid.
+	Verify(ski []byte, a, b, e *saferith.Int, S, T *saferith.Nat) bool
+}
