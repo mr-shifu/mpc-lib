7da2fcdf9a10b07fcb23a9eedd757913f579761d
hnamzian <namazian87@gmail.com>
2024-02-15 09:45:03 -0500

    Elgamal Key and keyManager interfaces defined in common

---
+++ pkg/common/cryptosuite/elgamal/elgamal.go
@@ -0,0 +1,31 @@
+package elgamal
+
+import "github.com/mr-shifu/mpc-lib/core/math/curve"
+
+type ElgamalKey interface {
+	// Bytes returns the byte representation of the key.
+	Bytes() ([]byte, error)
+
+	// SKI returns the serialized key identifier.
+	SKI() []byte
+
+	// Private returns true if the key is private.
+	Private() bool
+
+	// PublicKey returns the corresponding public key part of Elgamal Key.
+	PublicKey() ElgamalKey
+}
+
+type ElgamalKeyManger interface {
+	// GenerateKey generates a new Elgamal key pair.
+	GenerateKey() (ElgamalKey, error)
+
+	// Import imports a Elgamal key from its byte representation.
+	ImportKey(data []byte) (ElgamalKey, error)
+
+	// GetKey returns a Elgamal key by its SKI.
+	GetKey(ski []byte) (ElgamalKey, error)
+
+	// Encrypt returns the encryption of `message` as ciphertext and nonce.
+	Encrypt(ski []byte, message curve.Scalar) ([]byte, curve.Scalar)
+}
