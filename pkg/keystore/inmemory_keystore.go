28ac740b55beadc0aeb9067413748e1aa1bd99c8
hnamzian <namazian87@gmail.com>
2024-02-15 09:43:59 -0500

    InMemory keystore implemented in pkg/keystore

---
+++ pkg/keystore/inmemory_keystore.go
@@ -0,0 +1,48 @@
+package keystore
+
+import (
+	"errors"
+	"sync"
+)
+
+var (
+	ErrKeyNotFound = errors.New("key not found")
+)
+
+type InMemoryKeystore struct {
+	lock sync.RWMutex
+	keys map[string][]byte
+}
+
+func NewInMemoryKeystore() *InMemoryKeystore {
+	return &InMemoryKeystore{
+		keys: make(map[string][]byte),
+	}
+}
+
+func (store *InMemoryKeystore) Import(keyID string, key []byte) error {
+	store.lock.Lock()
+	defer store.lock.Unlock()
+
+	store.keys[keyID] = key
+	return nil
+}
+
+func (store *InMemoryKeystore) Get(keyID string) ([]byte, error) {
+	store.lock.RLock()
+	defer store.lock.RUnlock()
+
+	key, ok := store.keys[keyID]
+	if !ok {
+		return nil, ErrKeyNotFound
+	}
+	return key, nil
+}
+
+func (store *InMemoryKeystore) Delete(keyID string) error {
+	store.lock.Lock()
+	defer store.lock.Unlock()
+
+	delete(store.keys, keyID)
+	return nil
+}
