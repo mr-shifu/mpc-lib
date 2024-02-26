83b34fe02a4dfad5fd8d02e3ad0e37af308b9b5d
hnamzian <namazian87@gmail.com>
2024-02-26 17:46:08 -0500

    Created MPCKeygen struct that initialized by KeyManagers and passed them to round1 at Start function

--- protocols/cmp/keygen/keygen.go
+++ protocols/cmp/keygen/keygen.go
@@ -5,24 +5,103 @@ import (
 	"fmt"
 
 	"github.com/mr-shifu/mpc-lib/core/math/curve"
-	"github.com/mr-shifu/mpc-lib/core/math/polynomial"
 	"github.com/mr-shifu/mpc-lib/core/math/sample"
 	"github.com/mr-shifu/mpc-lib/core/party"
 	"github.com/mr-shifu/mpc-lib/core/pool"
 	"github.com/mr-shifu/mpc-lib/core/protocol"
 	"github.com/mr-shifu/mpc-lib/lib/round"
+
+	sw_elgamal "github.com/mr-shifu/mpc-lib/pkg/cryptosuite/sw/elgamal"
+	comm_elgamal "github.com/mr-shifu/mpc-lib/pkg/mpc/common/elgamal"
+	mpc_elgamal "github.com/mr-shifu/mpc-lib/pkg/mpc/elgamal"
+
+	sw_paillier "github.com/mr-shifu/mpc-lib/pkg/cryptosuite/sw/paillier"
+	comm_paillier "github.com/mr-shifu/mpc-lib/pkg/mpc/common/paillier"
+	mpc_paillier "github.com/mr-shifu/mpc-lib/pkg/mpc/paillier"
+
+	sw_pedersen "github.com/mr-shifu/mpc-lib/pkg/cryptosuite/sw/pedersen"
+	comm_pedersen "github.com/mr-shifu/mpc-lib/pkg/mpc/common/pedersen"
+	mpc_pedersen "github.com/mr-shifu/mpc-lib/pkg/mpc/pedersen"
+
+	sw_rid "github.com/mr-shifu/mpc-lib/pkg/cryptosuite/sw/rid"
+	comm_rid "github.com/mr-shifu/mpc-lib/pkg/mpc/common/rid"
+	mpc_rid "github.com/mr-shifu/mpc-lib/pkg/mpc/rid"
+
+	sw_vss "github.com/mr-shifu/mpc-lib/pkg/cryptosuite/sw/vss"
+	comm_vss "github.com/mr-shifu/mpc-lib/pkg/mpc/common/vss"
+	mpc_vss "github.com/mr-shifu/mpc-lib/pkg/mpc/vss"
+
+	inmem_keyrepo "github.com/mr-shifu/mpc-lib/pkg/keyrepository"
+	"github.com/mr-shifu/mpc-lib/pkg/keystore"
 	"github.com/mr-shifu/mpc-lib/protocols/cmp/config"
 )
 
 const Rounds round.Number = 5
 
-func Start(info round.Info, pl *pool.Pool, c *config.Config) protocol.StartFunc {
+type MPCKeygen struct {
+	elgamal_km  comm_elgamal.ElgamalKeyManager
+	paillier_km comm_paillier.PaillierKeyManager
+	pedersen_km comm_pedersen.PedersenKeyManager
+	vss_km      comm_vss.VssKeyManager
+	rid_km      comm_rid.RIDKeyManager
+	chainKey_km comm_rid.RIDKeyManager
+	// keys              map[string]round.Info
+	// roundStates       map[string]int
+}
+
+func NewMPCKeygen() *MPCKeygen {
+	pl := pool.NewPool(2)
+
+	elgamal_kr := inmem_keyrepo.NewKeyRepository()
+	elgamal_ks := keystore.NewInMemoryKeystore()
+	elgamal_km := sw_elgamal.NewElgamalKeyManager(elgamal_ks, &sw_elgamal.Config{Group: curve.Secp256k1{}})
+	elgamal := mpc_elgamal.NewElgamal(elgamal_km, elgamal_kr)
+
+	paillier_kr := inmem_keyrepo.NewKeyRepository()
+	paillier_ks := keystore.NewInMemoryKeystore()
+	paillier_km := sw_paillier.NewPaillierKeyManager(paillier_ks, pl)
+	paillier := mpc_paillier.NewPaillierKeyManager(paillier_km, paillier_kr)
+
+	pedersen_kr := inmem_keyrepo.NewKeyRepository()
+	pedersen_ks := keystore.NewInMemoryKeystore()
+	pedersen_km := sw_pedersen.NewPedersenKeymanager(pedersen_ks)
+	pedersen := mpc_pedersen.NewPedersenKeyManager(pedersen_km, pedersen_kr)
+
+	vss_kr := inmem_keyrepo.NewKeyRepository()
+	vss_ks := keystore.NewInMemoryKeystore()
+	vss_km := sw_vss.NewVssKeyManager(vss_ks, curve.Secp256k1{})
+	vss := mpc_vss.NewVSS(vss_km, vss_kr)
+
+	rid_kr := inmem_keyrepo.NewKeyRepository()
+	rid_ks := keystore.NewInMemoryKeystore()
+	rid_km := sw_rid.NewRIDManager(rid_ks)
+	rid := mpc_rid.NewRIDKeyManager(rid_km, rid_kr)
+
+	chainKey_kr := inmem_keyrepo.NewKeyRepository()
+	chainKey_ks := keystore.NewInMemoryKeystore()
+	chainKey_km := sw_rid.NewRIDManager(chainKey_ks)
+	chainKey := mpc_rid.NewRIDKeyManager(chainKey_km, chainKey_kr)
+
+	return &MPCKeygen{
+		elgamal_km:  elgamal,
+		paillier_km: paillier,
+		pedersen_km: pedersen,
+		vss_km:      vss,
+		rid_km:      rid,
+		chainKey_km: chainKey,
+	}
+
+}
+
+func (m *MPCKeygen) Start(keyID string, info round.Info, pl *pool.Pool, c *config.Config) protocol.StartFunc {
 	return func(sessionID []byte) (_ round.Session, err error) {
+		// m.keys[keyID] = info
+
 		var helper *round.Helper
 		if c == nil {
-			helper, err = round.NewSession(info, sessionID, pl)
+			helper, err = round.NewSession(keyID, info, sessionID, pl)
 		} else {
-			helper, err = round.NewSession(info, sessionID, pl, c)
+			helper, err = round.NewSession(keyID, info, sessionID, pl, c)
 		}
 		if err != nil {
 			return nil, fmt.Errorf("keygen: %w", err)
@@ -37,19 +116,35 @@ func Start(info round.Info, pl *pool.Pool, c *config.Config) protocol.StartFunc 
 			}
 			return &round1{
 				Helper:                    helper,
+				elgamal_km:                m.elgamal_km,
+				paillier_km:               m.paillier_km,
+				pedersen_km:               m.pedersen_km,
+				vss_km:                    m.vss_km,
+				rid_km:                    m.rid_km,
+				chainKey_km:               m.chainKey_km,
 				PreviousSecretECDSA:       c.ECDSA,
 				PreviousPublicSharesECDSA: PublicSharesECDSA,
 				PreviousChainKey:          c.ChainKey,
-				VSSSecret:                 polynomial.NewPolynomial(group, helper.Threshold(), group.NewScalar()), // fᵢ(X) deg(fᵢ) = t, fᵢ(0) = 0
 			}, nil
 		}
 
 		// sample fᵢ(X) deg(fᵢ) = t, fᵢ(0) = secretᵢ
 		VSSConstant := sample.Scalar(rand.Reader, group)
-		VSSSecret := polynomial.NewPolynomial(group, helper.Threshold(), VSSConstant)
+		if _, err = m.vss_km.GenerateSecrets(keyID, string(helper.SelfID()), VSSConstant, helper.Threshold()); err != nil {
+			return nil, fmt.Errorf("keygen: %w", err)
+		}
+
+		// set key round number
+		// m.roundStates[keyID] = 1
+
 		return &round1{
-			Helper:    helper,
-			VSSSecret: VSSSecret,
+			Helper:      helper,
+			elgamal_km:  m.elgamal_km,
+			paillier_km: m.paillier_km,
+			pedersen_km: m.pedersen_km,
+			vss_km:      m.vss_km,
+			rid_km:      m.rid_km,
+			chainKey_km: m.chainKey_km,
 		}, nil
 
 	}
