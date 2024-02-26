24faf54a1033b9cb03026fc3693608836963b307
hnamzian <namazian87@gmail.com>
2024-02-26 17:47:57 -0500

    Removed Serialzied/Desrialize funcs + Replaced keys by reading keys from Keymanager

--- protocols/cmp/keygen/round2.go
+++ protocols/cmp/keygen/round2.go
@@ -1,21 +1,16 @@
 package keygen
 
 import (
-	"bytes"
-	"encoding/json"
-
-	"github.com/cronokirby/saferith"
 	"github.com/mr-shifu/mpc-lib/core/hash"
 	"github.com/mr-shifu/mpc-lib/core/math/curve"
-	"github.com/mr-shifu/mpc-lib/core/math/polynomial"
-	"github.com/mr-shifu/mpc-lib/core/paillier"
 	"github.com/mr-shifu/mpc-lib/core/party"
-	"github.com/mr-shifu/mpc-lib/core/pedersen"
-	"github.com/mr-shifu/mpc-lib/core/pool"
-	zkmod "github.com/mr-shifu/mpc-lib/core/zk/mod"
 	zksch "github.com/mr-shifu/mpc-lib/core/zk/sch"
 	"github.com/mr-shifu/mpc-lib/lib/round"
-	"github.com/mr-shifu/mpc-lib/lib/types"
+	comm_elgamal "github.com/mr-shifu/mpc-lib/pkg/mpc/common/elgamal"
+	comm_paillier "github.com/mr-shifu/mpc-lib/pkg/mpc/common/paillier"
+	comm_pedersen "github.com/mr-shifu/mpc-lib/pkg/mpc/common/pedersen"
+	comm_rid "github.com/mr-shifu/mpc-lib/pkg/mpc/common/rid"
+	comm_vss "github.com/mr-shifu/mpc-lib/pkg/mpc/common/vss"
 )
 
 var _ round.Round = (*round2)(nil)
@@ -23,36 +18,44 @@ var _ round.Round = (*round2)(nil)
 type round2 struct {
 	*round1
 
+	elgamal_km  comm_elgamal.ElgamalKeyManager
+	paillier_km comm_paillier.PaillierKeyManager
+	pedersen_km comm_pedersen.PedersenKeyManager
+	vss_km      comm_vss.VssKeyManager
+	rid_km      comm_rid.RIDKeyManager
+	chainKey_km comm_rid.RIDKeyManager
+
 	// VSSPolynomials[j] = Fⱼ(X) = fⱼ(X)•G
-	VSSPolynomials map[party.ID]*polynomial.Exponent
+	// VSSPolynomials map[party.ID]*polynomial.Exponent
 
 	// Commitments[j] = H(Keygen3ⱼ ∥ Decommitments[j])
 	Commitments map[party.ID]hash.Commitment
 
 	// RIDs[j] = ridⱼ
-	RIDs map[party.ID]types.RID
+	// RIDs map[party.ID]types.RID
 	// ChainKeys[j] = cⱼ
-	ChainKeys map[party.ID]types.RID
+	// ChainKeys map[party.ID]types.RID
 
 	// ShareReceived[j] = xʲᵢ
 	// share received from party j
 	ShareReceived map[party.ID]curve.Scalar
 
-	ElGamalPublic map[party.ID]curve.Point
+	// ElgamalKeys map[party.ID]*cs_elgamal.ElgamalKey
+	// ElGamalPublic map[party.ID]curve.Point
+	// ElGamalSecret curve.Scalar
+
 	// PaillierPublic[j] = Nⱼ
-	PaillierPublic map[party.ID]*paillier.PublicKey
+	// PaillierPublic map[party.ID]*paillier.PublicKey
 
 	// Pedersen[j] = (Nⱼ,Sⱼ,Tⱼ)
-	Pedersen map[party.ID]*pedersen.Parameters
-
-	ElGamalSecret curve.Scalar
+	// Pedersen map[party.ID]*pedersen.Parameters
 
 	// PaillierSecret = (pᵢ, qᵢ)
-	PaillierSecret *paillier.SecretKey
+	// PaillierSecret *paillier.SecretKey
 
 	// PedersenSecret = λᵢ
 	// Used to generate the Pedersen parameters
-	PedersenSecret *saferith.Nat
+	// PedersenSecret *saferith.Nat
 
 	// SchnorrRand = aᵢ
 	// Randomness used to compute Schnorr commitment of proof of knowledge of secret share
@@ -103,16 +106,50 @@ func (r *round2) Finalize(out chan<- *round.Message) (round.Session, error) {
 		return nil, round.ErrNotEnoughMessages
 	}
 
+	// TODO need keyID to get the key
+	elgamalKey, err := r.elgamal_km.GetKey(r.KeyID, string(r.SelfID()))
+	if err != nil {
+		return nil, err
+	}
+	ekb, err := elgamalKey.Bytes()
+	if err != nil {
+		return nil, err
+	}
+
+	rid, err := r.rid_km.GetKey(r.KeyID, string(r.SelfID()))
+	if err != nil {
+		return nil, err
+	}
+
+	chainKey, err := r.chainKey_km.GetKey(r.KeyID, string(r.SelfID()))
+	if err != nil {
+		return nil, err
+	}
+
+	vsspoly, err := r.vss_km.GetKey(r.KeyID, string(r.SelfID()))
+	if err != nil {
+		return nil, err
+	}
+	exponents, err := vsspoly.ExponentsRaw()
+	if err != nil {
+		return nil, err
+	}
+
+	ped, err := r.pedersen_km.GetKey(r.KeyID, string(r.SelfID()))
+	if err != nil {
+		return nil, err
+	}
+
 	// Send the message we created in Round1 to all
-	err := r.BroadcastMessage(out, &broadcast3{
-		RID:                r.RIDs[r.SelfID()],
-		C:                  r.ChainKeys[r.SelfID()],
-		VSSPolynomial:      r.VSSPolynomials[r.SelfID()],
+	err = r.BroadcastMessage(out, &broadcast3{
+		RID:                rid.Raw(),
+		C:                  chainKey.Raw(),
+		VSSPolynomial:      exponents,
 		SchnorrCommitments: r.SchnorrRand.Commitment(),
-		ElGamalPublic:      r.ElGamalPublic[r.SelfID()],
-		N:                  r.Pedersen[r.SelfID()].N(),
-		S:                  r.Pedersen[r.SelfID()].S(),
-		T:                  r.Pedersen[r.SelfID()].T(),
+		ElGamalPublic:      ekb,
+		N:                  ped.PublicKeyRaw().N(),
+		S:                  ped.PublicKeyRaw().S(),
+		T:                  ped.PublicKeyRaw().T(),
 		Decommitment:       r.Decommitment,
 	})
 	if err != nil {
@@ -120,6 +157,12 @@ func (r *round2) Finalize(out chan<- *round.Message) (round.Session, error) {
 	}
 	return &round3{
 		round2:             r,
+		elgamal_km:         r.elgamal_km,
+		paillier_km:        r.paillier_km,
+		pedersen_km:        r.pedersen_km,
+		vss_km:             r.vss_km,
+		rid_km:             r.rid_km,
+		chainKey_km:        r.chainKey_km,
 		SchnorrCommitments: map[party.ID]*zksch.Commitment{},
 		MessageBroadcasted: make(map[party.ID]bool),
 	}, nil
@@ -144,332 +187,3 @@ func (round2) BroadcastContent() round.BroadcastContent { return &broadcast2{} }
 
 // Number implements round.Round.
 func (round2) Number() round.Number { return 2 }
-
-type round2Serialized struct {
-	Round1 []byte
-
-	VSSPolynomials map[party.ID][]byte
-
-	Commitments map[party.ID]hash.Commitment
-
-	RIDs map[party.ID]types.RID
-
-	ChainKeys map[party.ID]types.RID
-
-	ShareReceived map[party.ID][]byte
-
-	ElGamalPublic map[party.ID][]byte
-
-	PaillierPublic map[party.ID][]byte
-
-	Pedersen map[party.ID][]byte
-
-	ElGamalSecret []byte
-
-	PaillierSecret []byte
-
-	PedersenSecret []byte
-
-	SchnorrRand []byte
-
-	Decommitment hash.Decommitment
-
-	MessageBroadcasted map[party.ID]bool
-}
-
-func NewEmptyRound2(g curve.Curve, pl *pool.Pool) *round2 {
-	var r round2
-	r.round1 = NewEmptyRound1(g, pl)
-	r.VSSPolynomials = make(map[party.ID]*polynomial.Exponent)
-	r.ShareReceived = make(map[party.ID]curve.Scalar)
-	r.ElGamalPublic = make(map[party.ID]curve.Point)
-	r.PaillierPublic = make(map[party.ID]*paillier.PublicKey)
-	r.Pedersen = make(map[party.ID]*pedersen.Parameters)
-	return &r
-}
-
-func (r *round2) Serialize() (rss []byte, err error) {
-	rs := round2Serialized{
-		Commitments:        r.Commitments,
-		RIDs:               r.RIDs,
-		ChainKeys:          r.ChainKeys,
-		VSSPolynomials:     make(map[party.ID][]byte),
-		ShareReceived:      make(map[party.ID][]byte),
-		ElGamalPublic:      make(map[party.ID][]byte),
-		PaillierPublic:     make(map[party.ID][]byte),
-		Pedersen:           make(map[party.ID][]byte),
-		ElGamalSecret:      nil,
-		Decommitment:       r.Decommitment,
-		MessageBroadcasted: r.MessageBroadcasted,
-	}
-
-	rs.Round1, err = r.round1.Serialize()
-	if err != nil {
-		return nil, err
-	}
-
-	for id, poly := range r.VSSPolynomials {
-		rs.VSSPolynomials[id], err = poly.MarshalBinary()
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	for id, share := range r.ShareReceived {
-		rs.ShareReceived[id], err = share.MarshalBinary()
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	for id, ep := range r.ElGamalPublic {
-		rs.ElGamalPublic[id], err = ep.MarshalBinary()
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	for id, pp := range r.PaillierPublic {
-		rs.PaillierPublic[id], err = pp.Serialize()
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	for id, p := range r.Pedersen {
-		rs.Pedersen[id], err = p.Serialize()
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	rs.ElGamalSecret, err = r.ElGamalSecret.MarshalBinary()
-	if err != nil {
-		return nil, err
-	}
-
-	rs.PaillierSecret, err = r.PaillierSecret.Serialize()
-	if err != nil {
-		return nil, err
-	}
-
-	rs.PedersenSecret, err = r.PedersenSecret.MarshalBinary()
-	if err != nil {
-		return nil, err
-	}
-
-	rs.SchnorrRand, err = r.SchnorrRand.Serialize()
-	if err != nil {
-		return nil, err
-	}
-
-	return json.Marshal(rs)
-}
-
-func (r *round2) Deserialize(data []byte) (err error) {
-	var rs round2Serialized
-	if err := json.Unmarshal(data, &rs); err != nil {
-		return err
-	}
-
-	r.Commitments = rs.Commitments
-	r.RIDs = rs.RIDs
-	r.ChainKeys = rs.ChainKeys
-	r.MessageBroadcasted = rs.MessageBroadcasted
-	r.Decommitment = rs.Decommitment
-
-	if err := r.round1.Deserialize(rs.Round1); err != nil {
-		return err
-	}
-
-	for id, poly := range rs.VSSPolynomials {
-		rpoly := polynomial.NewEmptyExponent(r.Helper.Group())
-		if err := rpoly.UnmarshalBinary(poly); err != nil {
-			return err
-		}
-		r.VSSPolynomials[id] = rpoly
-	}
-
-	for id, share := range rs.ShareReceived {
-		rshare := curve.Secp256k1Scalar{}
-		if err := rshare.UnmarshalBinary(share); err != nil {
-			return err
-		}
-		r.ShareReceived[id] = &rshare
-	}
-
-	for id, ep := range rs.ElGamalPublic {
-		rep := curve.Secp256k1Point{}
-		if err := rep.UnmarshalBinary(ep); err != nil {
-			return err
-		}
-		r.ElGamalPublic[id] = &rep
-	}
-
-	for id, pp := range rs.PaillierPublic {
-		var paillierPubKey paillier.PublicKey
-		if err := paillierPubKey.Deserialize(pp); err != nil {
-			return err
-		}
-		r.PaillierPublic[id] = &paillierPubKey
-	}
-
-	for id, p := range rs.Pedersen {
-		var pedersen pedersen.Parameters
-		if err := pedersen.Deserialize(p); err != nil {
-			return err
-		}
-		r.Pedersen[id] = &pedersen
-	}
-
-	es := curve.Secp256k1Scalar{}
-	if err := es.UnmarshalBinary(rs.ElGamalSecret); err != nil {
-		return err
-	}
-	r.ElGamalSecret = &es
-
-	var paillierSecret paillier.SecretKey
-	if err := paillierSecret.Deserialize(rs.PaillierSecret); err != nil {
-		return err
-	}
-	r.PaillierSecret = &paillierSecret
-
-	pedersenSecret := saferith.Nat{}
-	if err := pedersenSecret.UnmarshalBinary(rs.PedersenSecret); err != nil {
-		return err
-	}
-	r.PedersenSecret = &pedersenSecret
-
-	schnorRand := zksch.Randomness{}
-	if err := schnorRand.Deserialize(rs.SchnorrRand); err != nil {
-		return err
-	}
-	r.SchnorrRand = &schnorRand
-
-	return nil
-}
-
-func (r *round2) Equal(rr round.Round) bool {
-	rn := rr.(*round2)
-
-	commitment, decommitment, err := r.HashForID(r.SelfID()).Commit(
-		r.RIDs[r.SelfID()],
-		r.ChainKeys[r.SelfID()],
-		r.VSSPolynomials[r.SelfID()],
-		r.SchnorrRand.Commitment(),
-		r.ElGamalPublic[r.SelfID()],
-		r.Pedersen[r.SelfID()].N(),
-		r.Pedersen[r.SelfID()].S(),
-		r.Pedersen[r.SelfID()].T(),
-	)
-	if err != nil {
-		return false
-	}
-
-	verified := rn.HashForID(rn.SelfID()).Decommit(
-		commitment,
-		decommitment,
-		rn.RIDs[r.SelfID()],
-		rn.ChainKeys[rn.SelfID()],
-		rn.VSSPolynomials[rn.SelfID()],
-		rn.SchnorrRand.Commitment(),
-		rn.ElGamalPublic[rn.SelfID()],
-		rn.Pedersen[rn.SelfID()].N(),
-		rn.Pedersen[rn.SelfID()].S(),
-		rn.Pedersen[rn.SelfID()].T(),
-	)
-	if !verified {
-		return false
-	}
-
-	mod := zkmod.NewProof(r.Hash().Clone(), zkmod.Private{
-		P:   r.PaillierSecret.P(),
-		Q:   r.PaillierSecret.Q(),
-		Phi: r.PaillierSecret.Phi(),
-	}, zkmod.Public{N: r.PaillierPublic[r.SelfID()].N()}, r.Pool)
-	if mod.Verify(zkmod.Public{N: rn.Pedersen[r.SelfID()].N()}, rn.HashForID(r.SelfID()), r.Pool) {
-		return false
-	}
-
-	if !rn.round1.Equal(r.round1) {
-		return false
-	}
-	if len(r.VSSPolynomials) != len(rn.VSSPolynomials) {
-		return false
-	}
-	for id, poly := range r.VSSPolynomials {
-		if !poly.Equal(*r.VSSPolynomials[id]) {
-			return false
-		}
-	}
-	if len(r.Commitments) != len(rn.Commitments) {
-		return false
-	}
-	for id, commitment := range r.Commitments {
-		if !bytes.Equal(commitment, rn.Commitments[id]) {
-			return false
-		}
-	}
-	if len(r.RIDs) != len(rn.RIDs) {
-		return false
-	}
-	for id, rid := range r.RIDs {
-		if !bytes.Equal(rid, rn.RIDs[id]) {
-			return false
-		}
-	}
-	if len(r.ChainKeys) != len(rn.ChainKeys) {
-		return false
-	}
-	for id, chainKey := range r.ChainKeys {
-		if !bytes.Equal(chainKey, rn.ChainKeys[id]) {
-			return false
-		}
-	}
-	if len(r.ShareReceived) != len(rn.ShareReceived) {
-		return false
-	}
-	for id, share := range r.ShareReceived {
-		if !share.Equal(rn.ShareReceived[id]) {
-			return false
-		}
-	}
-	if len(r.ElGamalPublic) != len(rn.ElGamalPublic) {
-		return false
-	}
-	for id, ep := range r.ElGamalPublic {
-		if !ep.Equal(rn.ElGamalPublic[id]) {
-			return false
-		}
-	}
-	if len(r.PaillierPublic) != len(rn.PaillierPublic) {
-		return false
-	}
-	for id, pp := range r.PaillierPublic {
-		if !pp.Equal(rn.PaillierPublic[id]) {
-			return false
-		}
-	}
-	if len(r.Pedersen) != len(rn.Pedersen) {
-		return false
-	}
-	// for id, p := range r.Pedersen {
-	// 	if !p.Equal(rn.Pedersen[id]) {
-	// 		return false
-	// 	}
-	// }
-	if !r.ElGamalSecret.Equal(rn.ElGamalSecret) {
-		return false
-	}
-	// if !r.PaillierSecret.Equal(rn.PaillierSecret) {
-	// 	return false
-	// }
-	// if !r.PedersenSecret.Equal(rn.PedersenSecret) {
-	// 	return false
-	// }
-	if !bytes.Equal(r.Decommitment, rn.Decommitment) {
-		return false
-	}
-
-	return true
-}
