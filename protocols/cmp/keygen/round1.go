0123150131ac7a688a45631173cee0b45b6960e6
hnamzian <namazian87@gmail.com>
2024-02-26 17:47:42 -0500

    Removed Serialzied/Desrialize funcs + Replaced keys by reading keys from Keymanager

--- protocols/cmp/keygen/round1.go
+++ protocols/cmp/keygen/round1.go
@@ -2,26 +2,33 @@ package keygen
 
 import (
 	"crypto/rand"
-	"encoding/json"
 	"errors"
 
 	"github.com/mr-shifu/mpc-lib/core/hash"
 	"github.com/mr-shifu/mpc-lib/core/math/curve"
-	"github.com/mr-shifu/mpc-lib/core/math/polynomial"
-	"github.com/mr-shifu/mpc-lib/core/math/sample"
-	"github.com/mr-shifu/mpc-lib/core/paillier"
 	"github.com/mr-shifu/mpc-lib/core/party"
-	"github.com/mr-shifu/mpc-lib/core/pedersen"
-	"github.com/mr-shifu/mpc-lib/core/pool"
 	zksch "github.com/mr-shifu/mpc-lib/core/zk/sch"
 	"github.com/mr-shifu/mpc-lib/lib/round"
 	"github.com/mr-shifu/mpc-lib/lib/types"
+
+	comm_elgamal "github.com/mr-shifu/mpc-lib/pkg/mpc/common/elgamal"
+	comm_paillier "github.com/mr-shifu/mpc-lib/pkg/mpc/common/paillier"
+	comm_pedersen "github.com/mr-shifu/mpc-lib/pkg/mpc/common/pedersen"
+	comm_rid "github.com/mr-shifu/mpc-lib/pkg/mpc/common/rid"
+	comm_vss "github.com/mr-shifu/mpc-lib/pkg/mpc/common/vss"
 )
 
 var _ round.Round = (*round1)(nil)
 
 type round1 struct {
 	*round.Helper
+
+	elgamal_km  comm_elgamal.ElgamalKeyManager
+	paillier_km comm_paillier.PaillierKeyManager
+	pedersen_km comm_pedersen.PedersenKeyManager
+	vss_km      comm_vss.VssKeyManager
+	rid_km      comm_rid.RIDKeyManager
+	chainKey_km comm_rid.RIDKeyManager
 
 	// PreviousSecretECDSA = sk'ᵢ
 	// Contains the previous secret ECDSA key share which is being refreshed
@@ -43,7 +50,7 @@ type round1 struct {
 	// Polynomial from which the new secret shares are computed.
 	// Keygen:  fᵢ(0) = xⁱ
 	// Refresh: fᵢ(0) = 0
-	VSSSecret *polynomial.Polynomial
+	// VSSSecret *polynomial.Polynomial
 }
 
 // VerifyMessage implements round.Round.
@@ -64,35 +71,98 @@ func (r *round1) StoreMessage(round.Message) error { return nil }
 // - commit to message.
 func (r *round1) Finalize(out chan<- *round.Message) (round.Session, error) {
 	// generate Paillier and Pedersen
-	PaillierSecret := paillier.NewSecretKey(nil)
-	SelfPaillierPublic := PaillierSecret.PublicKey
-	SelfPedersenPublic, PedersenSecret := PaillierSecret.GeneratePedersen()
+	_, err := r.paillier_km.GenerateKey(r.KeyID, string(r.SelfID()))
+	if err != nil {
+		return nil, err
+	}
 
-	ElGamalSecret, ElGamalPublic := sample.ScalarPointPair(rand.Reader, r.Group())
+	// derive Pedersen from Paillier
+	pedersenKey, err := r.paillier_km.DerivePedersenKey(r.KeyID, string(r.SelfID()))
+	if err != nil {
+		return nil, err
+	}
+	pedersen_kb, err := pedersenKey.Bytes()
+	if err != nil {
+		return nil, err
+	}
+	r.pedersen_km.ImportKey(r.KeyID, string(r.SelfID()), pedersen_kb)
+	// PaillierSecret := paillier.NewSecretKey(nil)
+	// SelfPaillierPublic := PaillierSecret.PublicKey
+	// SelfPedersenPublic, PedersenSecret := PaillierSecret.GeneratePedersen()
+
+	// generate ElGamal key
+	elgamlKey, err := r.elgamal_km.GenerateKey(r.KeyID, string(r.SelfID()))
+	if err != nil {
+		return nil, err
+	}
+	// ElGamalSecret, ElGamalPublic := sample.ScalarPointPair(rand.Reader, r.Group())
 
 	// save our own share already so we are consistent with what we receive from others
-	SelfShare := r.VSSSecret.Evaluate(r.SelfID().Scalar(r.Group()))
+	vssKey, err := r.vss_km.GetKey(r.KeyID, string(r.SelfID()))
+	if err != nil {
+		return nil, err
+	}
+	SelfShare, err := vssKey.Evaluate(r.SelfID().Scalar(r.Group()))
+	if err != nil {
+		return nil, err
+	}
+	// SelfShare := r.VSSSecret.Evaluate(r.SelfID().Scalar(r.Group()))
 
 	// set Fᵢ(X) = fᵢ(X)•G
-	SelfVSSPolynomial := polynomial.NewPolynomialExponent(r.VSSSecret)
+	// SelfVSSPolynomial := polynomial.NewPolynomialExponent(r.VSSSecret)
 
 	// generate Schnorr randomness
 	SchnorrRand := zksch.NewRandomness(rand.Reader, r.Group(), nil)
 
 	// Sample RIDᵢ
-	SelfRID, err := types.NewRID(rand.Reader)
+	selfRID, err := r.rid_km.GenerateKey(r.KeyID, string(r.SelfID()))
 	if err != nil {
-		return r, errors.New("failed to sample Rho")
-	}
-	chainKey, err := types.NewRID(rand.Reader)
-	if err != nil {
-		return r, errors.New("failed to sample c")
+		return nil, err
 	}
 
+	// SelfRID, err := types.NewRID(rand.Reader)
+	// if err != nil {
+	// 	return r, errors.New("failed to sample Rho")
+	// }
+
+	chainKey, err := r.chainKey_km.GenerateKey(r.KeyID, string(r.SelfID()))
+	if err != nil {
+		return nil, err
+	}
+	// chainKey, err := types.NewRID(rand.Reader)
+	// if err != nil {
+	// 	return r, errors.New("failed to sample c")
+	// }
+
 	// commit to data in message 2
+	// TODO Hash func must be fixed to handle cryptosuite keys
+	pedersen_bytes, err := pedersenKey.Bytes()
+	if err != nil {
+		return nil, err
+	}
+	elgamal_bytes, err := elgamlKey.Bytes()
+	if err != nil {
+		return nil, err
+	}
+	// vssKey, err := r.vss_km.GetKey(r.KeyID, string(r.SelfID()))
+	// if err != nil {
+	// 	return nil, err
+	// }
+	vssKey_bytes, err := vssKey.Bytes()
+	if err != nil {
+		return nil, err
+	}
+	selfRID_bytes, err := selfRID.Bytes()
+	if err != nil {
+		return nil, err
+	}
+	chainKey_bytes, err := chainKey.Bytes()
+	if err != nil {
+		return nil, err
+	}
+
 	SelfCommitment, Decommitment, err := r.HashForID(r.SelfID()).Commit(
-		SelfRID, chainKey, SelfVSSPolynomial, SchnorrRand.Commitment(), ElGamalPublic,
-		SelfPedersenPublic.N(), SelfPedersenPublic.S(), SelfPedersenPublic.T())
+		selfRID_bytes, chainKey_bytes, vssKey_bytes, SchnorrRand.Commitment(), elgamal_bytes, pedersen_bytes)
 	if err != nil {
 		return r, errors.New("failed to commit")
 	}
@@ -106,18 +176,15 @@ func (r *round1) Finalize(out chan<- *round.Message) (round.Session, error) {
 
 	nextRound := &round2{
 		round1:             r,
-		VSSPolynomials:     map[party.ID]*polynomial.Exponent{r.SelfID(): SelfVSSPolynomial},
-		Commitments:        map[party.ID]hash.Commitment{r.SelfID(): SelfCommitment},
-		RIDs:               map[party.ID]types.RID{r.SelfID(): SelfRID},
-		ChainKeys:          map[party.ID]types.RID{r.SelfID(): chainKey},
+		elgamal_km:         r.elgamal_km,
+		paillier_km:        r.paillier_km,
+		pedersen_km:        r.pedersen_km,
+		vss_km:             r.vss_km,
+		rid_km:             r.rid_km,
+		chainKey_km:        r.chainKey_km,
 		ShareReceived:      map[party.ID]curve.Scalar{r.SelfID(): SelfShare},
-		ElGamalPublic:      map[party.ID]curve.Point{r.SelfID(): ElGamalPublic},
-		PaillierPublic:     map[party.ID]*paillier.PublicKey{r.SelfID(): SelfPaillierPublic},
-		Pedersen:           map[party.ID]*pedersen.Parameters{r.SelfID(): SelfPedersenPublic},
-		ElGamalSecret:      ElGamalSecret,
-		PaillierSecret:     PaillierSecret,
-		PedersenSecret:     PedersenSecret,
 		SchnorrRand:        SchnorrRand,
+		Commitments:        map[party.ID]hash.Commitment{r.SelfID(): SelfCommitment},
 		Decommitment:       Decommitment,
 		MessageBroadcasted: make(map[party.ID]bool),
 	}
@@ -137,91 +204,3 @@ func (round1) MessageContent() round.Content { return nil }
 
 // Number implements round.Round.
 func (round1) Number() round.Number { return 1 }
-
-type round1Serialized struct {
-	Helper                    []byte
-	PreviousSecretECDSA       []byte
-	PreviousPublicSharesECDSA map[party.ID][]byte
-	PreviousChainKey          []byte
-	VSSSecret                 []byte
-}
-
-func NewEmptyRound1(g curve.Curve, pl *pool.Pool) *round1 {
-	return &round1{
-		Helper:                    round.NewEmptyHelper(g, pl),
-		PreviousPublicSharesECDSA: nil,
-		PreviousSecretECDSA:       nil,
-	}
-}
-func (r round1) Serialize() (ser []byte, err error) {
-	rs := round1Serialized{
-		PreviousPublicSharesECDSA: make(map[party.ID][]byte),
-		PreviousChainKey:          []byte(r.PreviousChainKey),
-	}
-
-	rs.Helper, err = r.Helper.Serialize()
-	if err != nil {
-		return nil, err
-	}
-
-	if r.PreviousSecretECDSA != nil {
-		rs.PreviousSecretECDSA, err = r.PreviousSecretECDSA.MarshalBinary()
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	for id, pk := range r.PreviousPublicSharesECDSA {
-		rs.PreviousPublicSharesECDSA[id], err = pk.MarshalBinary()
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	rs.VSSSecret, err = r.VSSSecret.Serialize()
-	if err != nil {
-		return nil, err
-	}
-
-	return json.Marshal(rs)
-}
-func (r *round1) Deserialize(data []byte) error {
-	var rs round1Serialized
-	if err := json.Unmarshal(data, &rs); err != nil {
-		return err
-	}
-
-	if err := r.Helper.Deserialize(rs.Helper); err != nil {
-		return err
-	}
-
-	r.PreviousChainKey = rs.PreviousChainKey
-
-	if rs.PreviousSecretECDSA != nil {
-		if err := r.PreviousSecretECDSA.UnmarshalBinary(rs.PreviousSecretECDSA); err != nil {
-			return err
-		}
-	}
-
-	if len(rs.PreviousPublicSharesECDSA) > 0 {
-		r.PreviousPublicSharesECDSA = make(map[party.ID]curve.Point)
-		for id, pk := range rs.PreviousPublicSharesECDSA {
-			if err := r.PreviousPublicSharesECDSA[id].UnmarshalBinary(pk); err != nil {
-				return err
-			}
-		}
-	}
-
-	r.VSSSecret = polynomial.NewEmptyPolynomial(r.Helper.Group(), r.Helper.Threshold())
-	if rs.VSSSecret != nil {
-		if err := r.VSSSecret.Deserialize(rs.VSSSecret); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func (r *round1) Equal(other round.Round) bool {
-	return true
-}
