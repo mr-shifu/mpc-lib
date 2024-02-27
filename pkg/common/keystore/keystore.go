7e37b5ef6332ac1f844f66bb6b5d4480e0e34e56
hnamzian <namazian87@gmail.com>
2024-02-27 18:50:49 -0500

    Created KeyLinkedStore that internaly binds data to keyID

--- pkg/common/keystore/keystore.go
+++ pkg/common/keystore/keystore.go
@@ -4,4 +4,11 @@ type Keystore interface {
 	Import(keyID string, key []byte) error
 	Get(keyID string) ([]byte, error)
 	Delete(keyID string) error
+	WithKeyID(keyID string) KeyLinkedStore
+}
+
+type KeyLinkedStore interface {
+	Import(key []byte) error
+	Get() ([]byte, error)
+	Delete() error
 }
