5c200801bb37856a9606a6d1b04b054a85bb1981
hnamzian <namazian87@gmail.com>
2024-03-02 07:52:04 -0500

    Fixed ECDSAKeyManager interface name

--- pkg/mpc/common/ecdsa/ecdsa.go
+++ pkg/mpc/common/ecdsa/ecdsa.go
@@ -4,7 +4,7 @@ import (
 	comm_ecdsa "github.com/mr-shifu/mpc-lib/pkg/common/cryptosuite/ecdsa"
 )
 
-type ElgamalKeyManager interface {
+type ECDSAKeyManager interface {
 	GenerateKey(keyID string, partyID string) (comm_ecdsa.ECDSAKey, error)
 	ImportKey(keyID string, partyID string, key comm_ecdsa.ECDSAKey) error
 	GetKey(keyID string, partyID string) (comm_ecdsa.ECDSAKey, error)
