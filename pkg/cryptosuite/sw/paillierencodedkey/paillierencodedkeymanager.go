b19602d1f0012868d069840869ba4b4d8b674073
hnamzian <namazian87@gmail.com>
2024-04-04 12:39:42 -0400

    paillierencodedkey manager modified to use keystore and bind key optins to key vault contents

--- pkg/cryptosuite/sw/paillierencodedkey/paillierencodedkeymanager.go
+++ pkg/cryptosuite/sw/paillierencodedkey/paillierencodedkeymanager.go
@@ -1,8 +1,10 @@
 package paillierencodedkey
 
 import (
+	"github.com/google/uuid"
 	pek "github.com/mr-shifu/mpc-lib/pkg/common/cryptosuite/paillierencodedkey"
 	"github.com/mr-shifu/mpc-lib/pkg/common/keystore"
+	"github.com/mr-shifu/mpc-lib/pkg/common/keyopts"
 )
 
 type PaillierEncodedKeyManager struct {
@@ -15,20 +17,41 @@ func NewPaillierEncodedKeyManager(store keystore.Keystore) *PaillierEncodedKeyMa
 	}
 }
 
-func (k *PaillierEncodedKeyManager) Get(keyID string) (pek.PaillierEncodedKey, error) {
-	b, err := k.store.Get(keyID)
+func (k *PaillierEncodedKeyManager) Get(opts keyopts.Options) (pek.PaillierEncodedKey, error) {
+	b, err := k.store.Get(opts)
 	if err != nil {
 		return nil, err
 	}
 	return fromBytes(b)
 }
 
-func (k *PaillierEncodedKeyManager) Import(keyID string, key pek.PaillierEncodedKey) (pek.PaillierEncodedKey, error) {
+func (k *PaillierEncodedKeyManager) Import(raw interface{}, opts keyopts.Options) (pek.PaillierEncodedKey, error) {
+	var err error
+	var key PaillierEncodedKey
+
+	switch raw := raw.(type) {
+	case []byte:
+		key, err = fromBytes(raw)
+		if err != nil {
+			return PaillierEncodedKey{}, err
+		}
+	case PaillierEncodedKey:
+		key = raw
+	case *PaillierEncodedKey:
+		key = *raw
+	}
+
 	b, err := key.Bytes()
 	if err != nil {
 		return nil, err
 	}
-	err = k.store.Import(keyID, b)
+
+	// get key SKI and encode it to hex string as keyID
+	// ski := key.SKI()
+	// keyID := hex.EncodeToString(ski)
+	keyID := uuid.New().String()
+
+	err = k.store.Import(keyID, b, opts)
 	if err != nil {
 		return nil, err
 	}
