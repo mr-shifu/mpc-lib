aa5739ce2e1b7e5855ac3d781f09dafad6fd36a8
hnamzian <namazian87@gmail.com>
2024-04-04 12:32:15 -0400

    Keyrepository replaced by KeyOpts to bind key data to key options

---
+++ pkg/common/keyopts/keyopts.go
@@ -0,0 +1,29 @@
+package keyopts
+
+type KeyData struct {
+	PartyID string
+	SKI     string
+}
+
+type Options interface {
+	Set(kVs ...interface{}) error
+	Get(key string) (interface{}, bool)
+}
+
+// KeyOpts manages the storage of key metadata referred to by an ID (MPC KeyID).
+type KeyOpts interface {
+	// Import imports a key into the repository.
+	// ID is the MPC KeyID and key is the key metadata (ex. SKI, PartyID).
+	Import(data interface{}, opts Options) error
+
+	// Get returns the key metadata by related to MPC Key ID.
+	Get(opts Options) (*KeyData, error)
+
+	// Get returns the all keys' metadata by related to MPC Key ID.
+	GetAll(opts Options) (map[string]*KeyData, error)
+
+	// Delete deletes all keys' metadata by related to MPC Key ID.
+	DeleteAll(opts Options) error
+
+	Delete(opts Options) error
+}
