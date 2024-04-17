7b49bfc703ca5820a788d05aa9ad8d5c18d11ad1
hnamzian <namazian87@gmail.com>
2024-04-17 09:24:57 -0400

    Verify and Store round3 broadcasr message

--- protocols/frost/keygen/round3.go
+++ protocols/frost/keygen/round3.go
@@ -1,10 +1,19 @@
 package keygen
 
 import (
+	"errors"
+
 	"github.com/mr-shifu/mpc-lib/core/hash"
 	"github.com/mr-shifu/mpc-lib/core/math/curve"
 	"github.com/mr-shifu/mpc-lib/lib/round"
+	"github.com/mr-shifu/mpc-lib/pkg/common/cryptosuite/commitment"
+	"github.com/mr-shifu/mpc-lib/pkg/common/cryptosuite/ecdsa"
 	"github.com/mr-shifu/mpc-lib/pkg/common/cryptosuite/rid"
+	"github.com/mr-shifu/mpc-lib/pkg/common/cryptosuite/vss"
+	"github.com/mr-shifu/mpc-lib/pkg/keyopts"
+	"github.com/mr-shifu/mpc-lib/pkg/mpc/common/config"
+	"github.com/mr-shifu/mpc-lib/pkg/mpc/common/message"
+	"github.com/mr-shifu/mpc-lib/pkg/mpc/common/state"
 )
 
 type broadcast3 struct {
@@ -15,6 +24,76 @@ type broadcast3 struct {
 type message3 struct {
 	VSSShare curve.Scalar
 }
+
+type round3 struct {
+	*round.Helper
+
+	configmgr   config.KeyConfigManager
+	statemgr    state.MPCStateManager
+	msgmgr      message.MessageManager
+	bcstmgr     message.MessageManager
+	ec_km       ecdsa.ECDSAKeyManager
+	ec_vss_km   ecdsa.ECDSAKeyManager
+	vss_mgr     vss.VssKeyManager
+	chainKey_km rid.RIDManager
+	commit_mgr  commitment.CommitmentManager
+}
+
+// StoreBroadcastMessage implements round.BroadcastRound.
+func (r *round3) StoreBroadcastMessage(msg round.Message) error {
+	from := msg.From
+	body, ok := msg.Content.(*broadcast3)
+	if !ok || body == nil {
+		return round.ErrInvalidContent
+	}
+
+	fromOpts := keyopts.Options{}
+	fromOpts.Set("id", r.ID, "partyid", from)
+
+	// 1. Validate ChainKey and Decommitment
+	if err := body.ChainKey.Validate(); err != nil {
+		return err
+	}
+	if err := body.Decommitment.Validate(); err != nil {
+		return err
+	}
+
+	// ToDo Decommit() can be embedded in commit manager
+	// 2. Verify the decommitment against chainKey
+	cmt, err := r.commit_mgr.Get(fromOpts)
+	if err != nil {
+		return err
+	}
+	if !r.Hash().Clone().Decommit(
+		cmt.Commitment(),
+		body.Decommitment,
+		body.ChainKey,
+	) {
+		return errors.New("failed to decommit")
+	}
+
+	// 3. Import the decommitment
+	if err := r.commit_mgr.ImportDecommitment(body.Decommitment, fromOpts); err != nil {
+		return err
+	}
+
+	// 5. Import the chainKey
+	if _, err := r.chainKey_km.ImportKey(body.ChainKey.Raw(), fromOpts); err != nil {
+		return err
+	}
+
+	// Mark the message as received
+	if err := r.bcstmgr.Import(
+		r.bcstmgr.NewMessage(r.ID, int(r.Number()), string(msg.From), true),
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Number implements round.Round.
+func (round3) Number() round.Number { return 2 }
 
 // RoundNumber implements round.Content.
 func (broadcast3) RoundNumber() round.Number { return 3 }
