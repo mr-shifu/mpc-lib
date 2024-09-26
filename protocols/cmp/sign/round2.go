bfd151d2b16b80b506350dcf1bb0453e55027e45
hnamzian <namazian87@gmail.com>
2024-09-26 15:41:21 -0400

    Fixed bug to update round state

--- protocols/cmp/sign/round2.go
+++ protocols/cmp/sign/round2.go
@@ -329,7 +329,7 @@ func (r *round2) Finalize(out chan<- *round.Message) (round.Session, error) {
 	}
 
 	// update last round processed in StateManager
-	if err := r.statemgr.SetLastRound(r.ID, int(r.Number())); err != nil {
+	if err := r.statemgr.SetLastRound(r.cfg.ID(), int(r.Number())); err != nil {
 		return r, err
 	}
 
